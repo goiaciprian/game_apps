08926799f373609cde5ad0e7a94462da60149f0f
Goia Ciprian <goia.ciprian14@gmail.com>
2022-08-09 18:09:57 +0300

    Update JwtProperties.java
    Removed secret key

--- GameAPI/src/main/java/com/gameapi/Configs/JwtProperties.java
+++ GameAPI/src/main/java/com/gameapi/Configs/JwtProperties.java
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "jwt")
 @Data
 public class JwtProperties {
-    private String secretKey = "T6RbpIlnicyWqHEH9GRoAf5TaATJgJeu`";
+    private String secretKey = "`";
     //validity in milliseconds
     private long validityInMs = 3600000; // 1h
 }
